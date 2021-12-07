19a38a7e09ff6c20127ef59024544b1f66632af2
argiandi <argiandi19a@gmail.comgit config --global user.email argiandi19a@gmail.com>
2021-12-07 00:14:53 -0800

    pertemuan12

---
+++ app/src/main/java/com/Argi19100007/projectargi/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.Argi19100007.projectargi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
